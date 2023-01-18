366c02b2ff75fa55127aac75fcd0ae44c9ed3cff
Anton Protasov <antonprotasow69@gmail.com>
2023-01-18 05:25:42 +0100

    s

---
+++ src/services/reducers/ingredient-details.js
@@ -0,0 +1,30 @@
+import { CURRENT_INGREDIENT_DETAILS } from '../actions/ingredient-details';
+
+
+const initialState = {
+
+    current: [],
+  
+}
+
+export const ingredientDetailsReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+
+        case CURRENT_INGREDIENT_DETAILS: {
+
+            return {
+                ...state,
+                current: action.product,
+               
+            }
+        }
+        default: {
+            return {
+                ...state
+            }
+        }
+    }
+
+
+}
